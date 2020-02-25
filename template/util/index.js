3d63a38eca5d60052d0dc54ef4e021a7ab3f8bf9
Tumble <tumblegamer@gmail.com>
2020-02-25 19:23:49 +0000

    fix

--- template/util/index.js
+++ template/util/index.js
@@ -24,7 +24,7 @@ module.exports = _.extend({
  */
 function view(name, model, cb) {
   //var basePath = path.join(path.dirname(require.main.filename), '../views');
-  var basePath = path.join(path.dirname(process.cwd(), '../views');
+  var basePath = path.join(path.dirname(process.cwd(), '../views'));
   var render = function(err, files) {
     if (err) {
       cb(err);
